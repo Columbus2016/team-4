c615e82ecb9672be1e7cd7ca7ef95c91730f4096
kingsman142 <jamhahn@yahoo.com>
2016-10-15 09:47:39 -0400

    implemented function to check whether a code is valid #MakeCodeGreatAgain

--- code-for-good/app/src/main/java/team4/code_for_good/CodeManager.java
+++ code-for-good/app/src/main/java/team4/code_for_good/CodeManager.java
@@ -1,5 +1,7 @@
 package team4.code_for_good;
 
+import java.util.HashMap;
+
 /**
  * Created by jameshahn on 10/15/16.
  */
@@ -10,12 +12,27 @@ public class CodeManager {
             return false;
         }
 
+        System.out.println("User's Code: " + code);
+
+        HashMap<Integer, HashMap<String, String>> redeemCodes = DatabaseQuery.getRedeemCodeData();
+
+        for(Integer key: redeemCodes.keySet()){
+            String currentCode = redeemCodes.get(key).get("redeem_code");
+            System.out.println("Code: " + code);
+
+            if(currentCode.equals(code)){
+                System.out.println("Valid code!");
+                return true;
+            }
+        }
         //Get code data
         //Find product name and foreign key it with the ProductsInfo table to find the points value
         //Return it here
         //Remove that code from the Codes table
         //Add the points of the product to the user's points value in the PersonalInfo table
-        return true;
+
+        System.out.println("Code was not found.");
+        return false;
     }
 
 }
