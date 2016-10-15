40edcc670b5e43d3e9089d07d939e5816e7f58c9
Vaughn Kottler <vkottler@wisc.edu>
2016-10-14 22:29:35 -0500

    updating products.php

--- products.php
+++ products.php
@@ -2,6 +2,11 @@
 
 include 'server.php';
 
+  $result = $conn->query("SELECT * FROM PointsLookup;");
 
+  while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+      echo $row . "<br>";
+  }
 
+  $result->close();
 ?>
