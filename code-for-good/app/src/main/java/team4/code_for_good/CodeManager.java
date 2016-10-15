e466aef1540a8187528a48d201c90768663bf244
kingsman142 <jamhahn@yahoo.com>
2016-10-15 07:21:11 -0400

    laid out how to redeem code once we get the buttons

---
+++ code-for-good/app/src/main/java/team4/code_for_good/CodeManager.java
@@ -0,0 +1,17 @@
+package team4.code_for_good;
+
+/**
+ * Created by jameshahn on 10/15/16.
+ */
+
+public class CodeManager {
+    public static boolean redeemCode(String code){
+        //Get code data
+        //Find product name and foreign key it with the ProductsInfo table to find the points value
+        //Return it here
+        //Remove that code from the Codes table
+        //Add the points of the product to the user's points value in the PersonalInfo table
+        return true;
+    }
+
+}
