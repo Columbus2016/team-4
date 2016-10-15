b614da2fc76d130d0a5ad2f133123bb042d03f3a
kingsman142 <jamhahn@yahoo.com>
2016-10-15 00:49:58 -0400

    commented a URL oject in databasequery.java

--- code-for-good/app/src/main/java/team4/code_for_good/DatabaseQuery.java
+++ code-for-good/app/src/main/java/team4/code_for_good/DatabaseQuery.java
@@ -4,10 +4,15 @@ package team4.code_for_good;
  * Created by jameshahn on 10/14/16.
  */
 
+import java.net.URL;
+
 public class DatabaseQuery {
     public static boolean emailAndPasswordMatch(String email, String password){
         //HttpPost post = new HttpPost("vaughnsplayground.me");
         //boolean match = post.execute("login.php?email=" + email + "&password=" + password);
         //return match;
+        //URL url = new URL("http://www.vaughnsplayground.me/codeForGood/login.php");
+
+        return true;
     }
 }
