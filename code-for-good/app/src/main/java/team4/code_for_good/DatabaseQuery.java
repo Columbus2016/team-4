655830e7b7daa66d3440decfb26f958c86ff3338
kingsman142 <jamhahn@yaho.com>
2016-10-14 23:11:12 -0400

    added method to test if email and password match; not implemented yet #MakeCodeGreatAgain

--- code-for-good/app/src/main/java/team4/code_for_good/DatabaseQuery.java
+++ code-for-good/app/src/main/java/team4/code_for_good/DatabaseQuery.java
@@ -5,5 +5,9 @@ package team4.code_for_good;
  */
 
 public class DatabaseQuery {
-
+    public static boolean emailAndPasswordMatch(String email, String password){
+        //HttpPost post = new HttpPost("vaughnsplayground.me");
+        //boolean match = post.execute("login.php?email=" + email + "&password=" + password);
+        //return match;
+    }
 }
