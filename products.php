cb93688e0b9b6290056055362e545c933e5c1eca
Vaughn Kottler <vkottler@razorScooter>
2016-10-15 03:46:28 +0000

    It's almost ready

--- products.php
+++ products.php
@@ -5,7 +5,10 @@ include 'server.php';
   $result = $conn->query("SELECT * FROM PointsLookup;");
 
   while ($row = $result->fetch_assoc()) {
-      echo var_dump($row) . "<br>";
+	$keys = array_keys($row);
+	for ($i = 0; $i < count($keys); $i++)
+		echo $row[$keys[$i]] . " "; 
+	echo "<br>";
   }
 
   $result->close();
