836078bd75c4e4fd8451ff7b1d7d6ad7e5da1328
kingsman142 <jamhahn@yaho.com>
2016-10-15 00:10:33 -0400

    went back to old code because it turns out it just wasn't being pushed to the server #MakeCodeGreatAgain

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
-    /*include 'server.php';
+    include 'server.php';
 
-    $sql = "SELECT * FROM PersonalInfo;";
+    $sql = "SELECT *
+            FROM PersonalInfo;";
 
     $result = $conn->query($sql);
 
@@ -13,14 +14,5 @@
         echo "<br>";
     }
 
-    $result->close();*/
-    include 'server.php';
-  $result = $conn->query("SELECT * FROM PointsLookup;");
-  while ($row = $result->fetch_assoc()) {
-	$keys = array_keys($row);
-	for ($i = 0; $i < count($keys); $i++)
-		echo $row[$keys[$i]] . " ";
-	echo "<br>";
-  }
-  $result->close();
+    $rconn->close();
  ?>
