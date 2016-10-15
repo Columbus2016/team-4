b7e3cf0a13ef07c6fa5c455c182ea432049fb225
Sage Conger <Sage Conger>
2016-10-15 04:43:20 -0400

    added some workin buttons

--- code-for-good/app/src/main/java/team4/code_for_good/MainScreen.java
+++ code-for-good/app/src/main/java/team4/code_for_good/MainScreen.java
@@ -8,9 +8,44 @@ import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
 
+    public Button button2, button3, button4;
+
+    public void button2() {
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(MainScreen.this, Donate.class);
+            }
+        });
+    }
+
+    public void button3() {
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy2 = new Intent(MainScreen.this, Volunteering.class);
+            }
+        });
+    }
+
+    public void button4() {
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy3 = new Intent(MainScreen.this, Aboutus.class);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        button2();
+        button3();
+        button4();
     }
 }
